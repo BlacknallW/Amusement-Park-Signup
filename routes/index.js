862167c274ddc83c168ee695709f93875b284aae
William Blacknall <williamblacknall@gmail.com>
2019-10-23 23:22:27 -0400

    Finished product

---
+++ routes/index.js
@@ -0,0 +1,38 @@
+const express = require("express"),
+    router = express.Router(),
+    ParksModel = require("../models/parks");
+
+/* GET home page. */
+router.get("/", async (req, res, next) => {
+    const parkList = await ParksModel.getAll();
+
+    res.render("template", {
+        locals: {
+            title: "Time to shred bruh!",
+            parkData: parkList,
+            isLoggedIn: req.session.is_logged_in,
+            userName : req.session.first_name
+        },
+        partials: {
+            partial: "partial-index"
+        }
+    });
+});
+
+router.get("/:park_id", async (req, res, next) => {
+    const { park_id } = req.params;
+    const thePark = await ParksModel.getById(park_id);
+
+    res.render("template", {
+        locals: {
+            title: "This is one park",
+            parkData: thePark,
+            isLoggedIn: req.session.is_logged_in
+        },
+        partials: {
+            partial: "partial-single-park"
+        }
+    });
+});
+
+module.exports = router;
